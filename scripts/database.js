ce96522ede7ebc17f9eef66bce45036b62ca3d98
Iris Lee <irislee4git@gmail.com>
2024-04-16 11:24:44 -0400

    rest of code from dynamite duo chapters

--- scripts/database.js
+++ scripts/database.js
@@ -1,21 +1,3 @@
-const database = {
-    heroes: [
-        {
-            id: 1,
-            name: "Dynoguy",
-            power: "Fire blasts"
-        },
-        {
-            id: 2,
-            name: "Hyperspeed",
-            power: "Super speed"
-        }
-    ]
-}
-
-export const getHeroes = () => {
-    return database.heroes.map(hero => ({...hero}))
-}
 
 const database = {
     heroes: [
@@ -31,10 +13,23 @@ const database = {
         }
     ],
     villains: [
-        // Remove this comment and create some villain objects
+       {
+            id: 1, 
+            name: "Joker", 
+            power: "manipulation"
+       },
+       {
+            id: 2, 
+            name: "Harley Quinn",
+            power: "chaos"
+       }
     ]
 }
 
 export const getVillains = () => {
     return database.villains.map(villain => ({...villain}))
+}
+
+export const getHeroes = () => {
+    return database.heroes.map(hero => ({...hero}))
 }
